2b63439707795266c9a46a8ed220e1c84494f16e
TopMyster <topmyster@outlook.com>
2025-08-03 18:53:31 -0500

    adds base light and darkmode

--- script.js
+++ script.js
@@ -138,5 +138,22 @@ document.getElementById('userinput').addEventListener('click', function() {
     document.getElementById('userinput').value = ''
 })
 
+window.onload = function() {
+    let mode = localStorage.getItem("mode")
 
+     if (mode === "dark") {
+        document.body.style.backgroundColor = 'black'
+    } else if (mode === "light"){
+        document.body.style.backgroundColor = 'white'
+    }
+}
 
+let mode = localStorage.getItem("mode")
+
+     if (mode === "dark") {
+        document.body.style.backgroundColor = 'black'
+        mode = "dark"
+    } else if (mode === "light"){
+        document.body.style.backgroundColor = 'white'
+        mode = "light"
+    }
